f237f10a3b87c5e9bb3cca3767ff1ce1eb097772
st128051 <st128051@student.spbu.ru>
2024-09-21 23:57:03 +0400

    Update Sourse.cpp

--- 2024.09.21-HW-1/Project9/Sourse.cpp
+++ 2024.09.21-HW-1/Project9/Sourse.cpp
@@ -9,9 +9,10 @@ int main(int argc, char* argv[]) {
 	scanf_s("%d", &a);
 	scanf_s("%d", &b);
 
-	int x = ((h - b) / (a - b));
+	int x = h - b;
+	int y = a - b;
 
-	printf("%d", x - int(x < 0) * (x - 1) + int((((a - b) + ((h - b) % (a - b))) % (a - b)) > 0));//Оно честно прошло все тесты
+	printf("%d", 1 + (x-1)/y * int(x > 0));
 
 	return EXIT_SUCCESS;
 }
